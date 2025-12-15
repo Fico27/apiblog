ddb64f6fe9152905f378d73ea5197f72079b58b5
Phil <182430206+Fico27@users.noreply.github.com>
2025-12-15 15:56:26 +0100

    Updated PostContent.jsx to include possibilty of no post being found. added postId variable to dependency array of useEffect.

--- frontend-user/src/components/PostContent.jsx
+++ frontend-user/src/components/PostContent.jsx
@@ -1,12 +1,18 @@
 import { useState, useEffect, useParams } from "react";
 
 function PostContent() {
-  const [post, setPost] = useState([]);
+  const [post, setPost] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const { postId } = useParams();
 
   useEffect(() => {
+    if (!postId) {
+      setError("No post ID provided");
+      setLoading(false);
+      return;
+    }
+
     async function fetchPost() {
       try {
         const response = await fetch(`/api/posts/${postId}`);
@@ -21,14 +27,16 @@ function PostContent() {
     }
 
     fetchPost();
-  }, []);
+  }, [postId]);
 
   if (loading) return <p>Loading Post...</p>;
   if (error) return <p>Error loading posts: {error}</p>;
+  if (!post) return <p>Post not found!</p>;
 
   return (
     <div className="post-container">
       <h1>{post.title}</h1>
+      <p>By: {post.author}</p>
       <p>{post.content}</p>
     </div>
   );
