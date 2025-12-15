e197aaa4e038d5eedd7c7539f3f4bdd934c1e8fc
Phil <182430206+Fico27@users.noreply.github.com>
2025-12-15 10:16:55 +0100

    started building out navbar component. Will save more frequently as usb ports + keyboards randomly die until restart.

---
+++ frontend-user/src/components/NavBar.jsx
@@ -0,0 +1,21 @@
+function NavBar() {
+  return (
+    <div className="navbar-container">
+      <h2>I Am Your Blog</h2>
+
+      <div className="nav-container">
+        <ul>
+          <li>
+            <a href="">Home</a>
+          </li>
+          <li>
+            <a href="">Sign up</a>
+          </li>
+          <li>
+            <a href="">Log in</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
