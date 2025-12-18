9488467a895762d0c20621124879bcbc21f488e2
Phil <182430206+Fico27@users.noreply.github.com>
2025-12-18 14:37:36 +0100

    Started adding post edit functionality. Still need to rework some things.

--- frontend-admin/src/components/PostContent.jsx
+++ frontend-admin/src/components/PostContent.jsx
@@ -1,19 +1,19 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 function PostContent() {
   const [post, setPost] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const { postId } = useParams();
+  const [title, setTitle] = useState(post.title || "");
+  const [content, setContent] = useState(post.content || "");
+  const [published, setPublished] = useState(post.published || false);
+
+  const navigate = useNavigate();
 
   useEffect(() => {
-    if (!postId) {
-      setError("No post ID provided");
-      setLoading(false);
-      return;
-    }
-
     const token = localStorage.getItem("token");
 
     if (!token) {
@@ -46,26 +46,64 @@ function PostContent() {
   if (error) return <p>Error loading posts: {error}</p>;
   if (!post) return <p>Post not found!</p>;
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError("");
+    setLoading(true);
+
+    try {
+      // Change this to correct API ROUTE
+      const response = await fetch(`/admin/posts/${postId}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ title, content, published }),
+      });
+
+      if (!response.ok) {
+        const errData = await response.json();
+        throw new Error(errData.error || "Login Failed");
+      }
+
+      navigate("/posts");
+    } catch (err) {
+      setError(err.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <>
-      <div className="post-container">
-        <h1>{post.title}</h1>
-        <p>By: {post.author.username}</p>
-        <p>{post.content}</p>
-      </div>
+      <div className="edit-post-container">
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            id="title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            placeholder="Email"
+            required
+          />
 
-      <div className="comments-container">
-        <h2>Comments</h2>
-        {post.comments && post.comments.length > 0 ? (
-          post.comments.map((comment) => (
-            <div key={comment.id} className="comment-card">
-              <p className="comment-author">{comment.author.username}</p>
-              <p>{comment.content}</p>
-            </div>
-          ))
-        ) : (
-          <p>No comments yet. Be the first to comment!</p>
-        )}
+          <textarea
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+            placeholder="Write something here!"
+            required
+          />
+
+          <label htmlFor="published">Publish</label>
+          <input
+            type="checkbox"
+            id="published"
+            checked={published}
+            onChange={(e) => setPublished(e.target.checked)}
+          />
+
+          <button type="submit">Submit</button>
+        </form>
       </div>
     </>
   );
