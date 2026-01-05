17e24f39f833afc0b0b5d8c0e8b9decdbee2c9b0
Phil <182430206+Fico27@users.noreply.github.com>
2026-01-05 15:36:52 +0100

    Began building out google passport.

--- backend/src/config/passport.js
+++ backend/src/config/passport.js
@@ -1,1 +1,14 @@
+require("dotenv").config();
 const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const prisma = require("../lib/prisma");
+
+const { signToken } = require("../utils/jwt");
+
+passport.use(
+  new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_id,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: "/auth/google/callback",
+  })
+);
