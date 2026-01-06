df07f8c5ea44b1ca5a040f14a166cc85b536860b
Phil <182430206+Fico27@users.noreply.github.com>
2026-01-06 11:49:17 +0100

    Updated app.js to use express sessions and passport setup for OAuth.

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,18 +1,34 @@
 const express = require("express");
+const session = require("express-session");
 const postRoutes = require("./routes/postRoutes");
 const authRoutes = require("./routes/authRoutes");
 const commentRoutes = require("./routes/commentsRoutes");
 const googleAuthRouter = require("./routes/googleAuthRoutes");
+const passport = require("./config/passport");
 
 const app = express();
 app.use(express.json());
 
-//Setup quick test.
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      secure: false,
+      httpOnly: true,
+      maxAge: 1000 * 60 * 10,
+    },
+  })
+);
 
 app.use("/api", postRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/posts", commentRoutes);
 app.use("/auth", googleAuthRouter);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
