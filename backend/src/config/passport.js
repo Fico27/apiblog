292e32369146c7f6e19b0c602d03d25f0b35a5b9
Phil <182430206+Fico27@users.noreply.github.com>
2026-01-06 10:10:27 +0100

    added user creation to passport if no user exists.

--- backend/src/config/passport.js
+++ backend/src/config/passport.js
@@ -3,12 +3,50 @@ const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const prisma = require("../lib/prisma");
 
-const { signToken } = require("../utils/jwt");
-
 passport.use(
-  new GoogleStrategy({
-    clientID: process.env.GOOGLE_CLIENT_id,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "/auth/google/callback",
-  })
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_id,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: "/auth/google/callback",
+    },
+
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        //User email given by google.
+        const email = profile.emails[0].value;
+
+        // check if user already exists if it doesn't create a new user
+        let user = await prisma.user.findUnique({
+          where: { email },
+        });
+
+        //If no display name make it the start of the email
+        if (!user) {
+          user = await prisma.create({
+            data: {
+              email,
+              username: profile.displayName || email.split("@")[0],
+              role: "user",
+            },
+          });
+        }
+        done(null, user);
+      } catch (err) {
+        done(err);
+      }
+    }
+  )
 );
+
+passport.serializeUser((user, done) => done(null, user.id));
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await prisma.user.findUnique({ where: { id } });
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
+
+module.exports = passport;
